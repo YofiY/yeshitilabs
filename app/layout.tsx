c72b8e5770e326dddb540b69a861a95e2c3ebfb2
YofiY <theburnface2013@gmail.com>
2024-09-12 18:31:19 +0200

    removed unused import

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import Link from 'next/link'
-import Image from 'next/image'
+
 
 const inter = Inter({ subsets: ['latin'] })
 
